2276454961073981de118efa58d5a98cfa422c3e
sofyanthayf <sofyanthayf@gmail.com>
2022-01-18 13:42:32 +0800

    update

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -2,8 +2,8 @@ const key = "323ee6e4b1621c63a0a839a8e48ba899";
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
 
-function getListMovie() {
-    fetch(endpoint_url + 'movie/now_playing?api_key=' + key + '&language=en-US&page=1' )
+function getListMovie( service, sectiontitle) {
+    fetch(endpoint_url + service + "?api_key=" + key + '&language=en-US&page=1' )
     .then(status)
     .then(json)
     .then( function(data){
@@ -12,7 +12,7 @@ function getListMovie() {
         data.results.forEach( function(movie) {
             movieHTML += `
                 <div class="col m3 s6">
-                    <div class="card">
+                    <div class="card moviecard">
                         <a href="./movie.html?id=${movie.id}">
                             <div class="card-image waves-effect waves-block waves-light">
                             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
@@ -29,6 +29,7 @@ function getListMovie() {
         });
 
         document.getElementById("movie_list").innerHTML = movieHTML;
+        document.getElementById("section_title").innerHTML = sectiontitle;
     })
     .catch(error);
 }
